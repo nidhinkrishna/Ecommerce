a286d2a232f8d68b3d8c709ef41e78b82eb6b9bd
nidhinkrishna <nidhingopalank@gmail.com>
2023-06-18 21:23:15 +0530

    home page and brand model completed

---
+++ Ecom/brands/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+class Brands(models.Model):
+    brand_name = models.CharField(max_length=50,unique=True)
+    slug = models.CharField(max_length=100,unique=True)
+    description = models.TextField(max_length=255,blank=True)
+    Brand_image = models.ImageField(upload_to='photos/categories',blank=True)
+
+    class Meta:
+        verbose_name = 'Brands'
+        verbose_name_plural = 'Brands'
+    def __str__(self):
+        return self.brand_name
