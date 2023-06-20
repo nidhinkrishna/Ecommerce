651379d98193429e32d1476fdb0d1167116d4d46
nidhinkrishna <nidhingopalank@gmail.com>
2023-06-20 12:57:09 +0530

    some ui modifications in storepage is done

--- Ecom/Ecom/urls.py
+++ Ecom/Ecom/urls.py
@@ -23,5 +23,5 @@ urlpatterns = [
     path('',include('base.urls')),
     path('brands/',include('brands.urls')),
     path('accounts/',include('accounts.urls')),
-     path('store/',include('store.urls')),
+    path('store/',include('store.urls')),
 ] + static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
--- Ecom/store/urls.py
+++ Ecom/store/urls.py
@@ -3,6 +3,7 @@ from . import views
 from .views import store
 urlpatterns = [
     path('',views.store,name='store'),
+    path('store.html',views.store,name='store'),
     path('<slug:brands_slug>/',views.store,name='product_by_brand'),
     
 ]
