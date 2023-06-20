f4a27998e8a86ae394fffdfc3af391e2ca18b6b9
nidhinkrishna <nidhingopalank@gmail.com>
2023-06-20 10:54:00 +0530

    products added in store page

--- Ecom/store/urls.py
+++ Ecom/store/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 from .views import store
 urlpatterns = [
-    path('',views.store,name='store')
+    path('',views.store,name='store'),
+    path('<slug:brands_slug>/',views.store,name='products_by_brands'),
     
 ]
--- Ecom/store/views.py
+++ Ecom/store/views.py
@@ -1,6 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
+from brands.models import Brands
+from . models import *
 
 # Create your views here.
-def store(request):
+def store(request,brands_slug = None):
+    brands = None
+    products = None
+    if brands_slug != None:
+        brands = get_object_or_404(Brands,slug=brands_slug)
+        products = Products.objects.filter(brand_name = brands,is_available = True)
+        products_count = products.count()
+    else:
+        products= Products.objects.all()
+        products_count = products.count()
 
-    return render (request,'store/store.html')
+    context = {
+        'products':products,
+        'count':products_count
+    
+    }
+    
+
+    return render (request,'store/store.html',context)
