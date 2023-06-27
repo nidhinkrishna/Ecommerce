b31615aa361583213133fd53b2accb7a2a2f564c
nidhinkrishna <nidhingopalank@gmail.com>
2023-06-27 12:52:37 +0530

    rating in home page done

--- Ecom/base/views.py
+++ Ecom/base/views.py
@@ -1,13 +1,17 @@
 from django.shortcuts import render,HttpResponse
-from store.models import Products
+from store.models import Products,ReviewRating
 
 # Create your views here.
 def home(request):
 
-    trending = Products.objects.all().filter(is_available = True)
+    trending = Products.objects.all().filter(is_available = True).order_by('-created_date')
+
+    for trend in trending:
+        reviews = ReviewRating.objects.filter(product_id=trend.id, status=True)
 
     context = {
-        'trending':trending
+        'trending':trending,
+        'reviews':reviews
     }
 
     return render (request,'base/home.html',context)
